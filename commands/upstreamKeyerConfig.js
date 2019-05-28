dc7d106b8bea24cc000a78e7e11ec957535ccd38
Hayden <haydendonald@gmail.com>
2019-05-28 10:39:08 +1000

    Bug fix
    Sometimes the upstream keyers were not setting their key and fill sources correctly

--- commands/upstreamKeyerConfig.js
+++ commands/upstreamKeyerConfig.js
@@ -13,8 +13,16 @@ module.exports = {
   processData(data, flag, command, commandList) {
 
     //Put this data in the upstream keyer (Each keyerid is the meid + 1 * the keyer id + 1)
-    commandList.list.upstreamKeyer.addKeyerInformation(((data[0] * 10) + (data[1])), commandList.list.inputProperty.findInput(data.readUInt16BE(6)), 
-      commandList.list.inputProperty.findInput(data.readUInt16BE(8)), commandList);
+    // commandList.list.upstreamKeyer.addKeyerInformation(((data[0] * 10) + (data[1])), commandList.list.inputProperty.findInput(data.readUInt16BE(6)), 
+    //   commandList.list.inputProperty.findInput(data.readUInt16BE(8)), commandList);
+
+    var keyerID = ((data[0] * 10) + (data[1]));
+    this.data[keyerID] = {};
+    this.data[keyerID].fillSourceID = data.readUInt16BE(6);
+    this.data[keyerID].fillSource = commandList.list.inputProperty.findInput(data.readUInt16BE(6));
+    this.data[keyerID].keySourceID = data.readUInt16BE(8);
+    this.data[keyerID].keySource = commandList.list.inputProperty.findInput(data.readUInt16BE(8));
+    commandList.list.upstreamKeyer.addKeyerInformation(keyerID, this.data[keyerID.fillSource], this.data[keyerID.keySource], commandList);
     return true;
   },
   sendData(command, commandList) {
@@ -30,7 +38,15 @@ module.exports = {
     return msg;
   },
   //What todo once we are connected
-  afterInit() {
-    return false;
+  afterInit(commandList) {
+    for(var i in this.data) {
+      this.data[i].fillSource = commandList.list.inputProperty.findInput(this.data[i].fillSourceID);
+      this.data[i].keySource = commandList.list.inputProperty.findInput(this.data[i].keySourceID);
+      commandList.list.upstreamKeyer.addKeyerInformation(i, this.data[i].fillSource, this.data[i].keySource, commandList);
+    }
+    return {
+      "cmd": this.cmd,
+      "data": this.data
+    }
   }
 }
